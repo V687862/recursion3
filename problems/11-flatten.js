80ba1beb953a3a0b918215f3f2227f549da72311
Yasuhiro Sekiyama <sekiyamayasuhiro@gmail.com>
2023-08-24 20:00:51 -0700

    problem11

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -12,10 +12,27 @@ flatten([1, 2]); // [1, 2]
 flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
+// function flatten(arr) {
+//   let result = [];
+
+//   for (let item of arr) {
+//     result = Array.isArray(item) ? result.concat(flatten(item)) : result.concat(item);
+//   }
+
+//   return result;
+
+// }
+
 function flatten(arr) {
-  // Your code here 
+  if (arr.length === 0) {
+    return [];
+  };
+  if (Array.isArray(arr[0])) {
+    return flatten(arr[0]).concat(flatten(arr.slice(1)))
+  } else {
+    return [arr[0]].concat(flatten(arr.slice(1)));
+  }
 }
-  
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
